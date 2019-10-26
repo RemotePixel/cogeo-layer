e24d4df2562c7c96a787aef214dcd50b56dbc9bd
vincentsarago <vincent.sarago@gmail.com>
2019-10-26 12:33:36 +0000

    fix tests

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -13,15 +13,18 @@ import rio_tiler_mvt
 import shapely
 import supermercado
 
-assert lambda_proxy.version
-assert pyproj.__version__
-assert numpy.__version__
-assert rasterio.__version__
-assert requests.__version__
-assert rio_cogeo.version
-assert rio_color.__version__
-assert rio_tiler.version
-assert rio_tiler_mosaic.version
-assert rio_tiler_mvt.__version__
-assert shapely.__version__
-assert supermercado.__package__
+
+def test_modules():
+    """Test we can import modules."""
+    assert lambda_proxy.version
+    assert pyproj.__version__
+    assert numpy.__version__
+    assert rasterio.__version__
+    assert requests.__version__
+    assert rio_cogeo.version
+    assert rio_color.__version__
+    assert rio_tiler.version
+    assert rio_tiler_mosaic.version
+    assert rio_tiler_mvt.__version__
+    assert shapely.__version__
+    assert supermercado.__package__
